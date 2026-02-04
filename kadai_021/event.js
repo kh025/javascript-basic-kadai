d337b57322c2bbc7a9f313c5bd20d761340daa53
kh025 <komachi787@gmail.com>
2026-02-04 20:35:21 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn');
+
+const h2 = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+    h2.textContent = 'ボタンをクリックしました';
+    console.log(h2);
+  }, 2000);
+});
